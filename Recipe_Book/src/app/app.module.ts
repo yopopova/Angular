8b1318aa24ace3cb7627e7875adb5488ca8d825e
Yoanna Popova <yoanna.dp@gmail.com>
2024-12-03 12:08:08 +0200

    Set up store data functionality.

--- Recipe_Book/src/app/app.module.ts
+++ Recipe_Book/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,6 +36,7 @@ import { RecipeService } from './recipes/recipe.service';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule,
   ],
   providers: [ShoppingListService, RecipeService],
