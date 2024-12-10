4db010141d2bbd223bbf90ca4ea4e3556c10ed8f
Yoanna Popova <yoanna.dp@gmail.com>
2024-12-10 13:30:57 +0200

    Set up error handling.

--- Recipe_Book/src/app/auth/auth.service.ts
+++ Recipe_Book/src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { catchError, throwError } from "rxjs";
 
 export interface AuthResponseData {
@@ -22,20 +22,8 @@ export class AuthService {
             email: email,
             password: password,
             returnSecureToken: true
-        }).pipe(catchError(errorRes => {
-            let errorMessage = 'An unknown error occured!';
-
-            if (!errorRes.error || !errorRes.error.error) {
-                return throwError(errorMessage);
-            }
-
-            switch (errorRes.error.error.message) {
-                case 'EMAIL_EXISTS':
-                    errorMessage = 'The email address is already in use by another account.'
-            }
-
-            return throwError(errorMessage);
-        }))
+        })
+        .pipe(catchError(this.handleError))
     }
 
     login(email: string, password: string) {
@@ -44,5 +32,28 @@ export class AuthService {
             password: password,
             returnSecureToken: true
         })
+        .pipe(catchError(this.handleError))
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occured!';
+
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+        }
+
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'The email address is already in use by another account.';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This email does not exist.';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'This password is not correct.';
+                break;
+        }
+
+        return throwError(errorMessage);
     }
 }
