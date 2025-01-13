18e6ad654d72f0f3d207e8f4975c9544637aa1c6
Yoanna Popova <yoanna.dp@gmail.com>
2025-01-13 14:48:00 +0200

    Set up user authentication into the header component.

--- Recipe_Book/src/app/header/header.component.ts
+++ Recipe_Book/src/app/header/header.component.ts
@@ -1,13 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 
 import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
 
 @Component({
     selector: 'app-header',
     templateUrl: 'header.component.html',
 })
-export class HeaderComponent {
-    constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+    isAuthenticated = false;
+    private userSub!: Subscription;
+
+    constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+
+    ngOnInit(): void {
+        this.userSub = this.authService.user.subscribe(user => {
+            // this.isAuthenticated = !user ? false : true;
+            this.isAuthenticated = !!user; // This is the same as the line above.
+            console.log(!user);
+            console.log(!!user);            
+        });
+    }
 
     onSaveData() {
         this.dataStorageService.storeRecipes();
@@ -15,5 +29,9 @@ export class HeaderComponent {
 
     onFetchData() {
         this.dataStorageService.fetchRecipes().subscribe();
+    }
+
+    ngOnDestroy(): void {
+        this.userSub.unsubscribe();
     }
 }
