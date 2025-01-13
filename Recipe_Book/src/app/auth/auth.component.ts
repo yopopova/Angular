ee96612a7addc24fd36a85a75a510450ca9f7419
Yoanna Popova <yoanna.dp@gmail.com>
2025-01-13 14:48:32 +0200

    Set up auth component logic.

--- Recipe_Book/src/app/auth/auth.component.ts
+++ Recipe_Book/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 
 import { AuthResponseData, AuthService } from "./auth.service";
@@ -11,9 +12,9 @@ import { AuthResponseData, AuthService } from "./auth.service";
 export class AuthComponent {
     isLoginMode: boolean = true;
     isLoading: boolean = false;
-    error!: string;
+    error: string = null!;
 
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private router: Router) {}
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -41,6 +42,7 @@ export class AuthComponent {
             resData => {
                 console.log(resData);
                 this.isLoading = false;
+                this.router.navigate(['/recipes']);
             },
             errorMessage => {
                 console.log(errorMessage);
